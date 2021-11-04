51cfeaa5c1af528314ba632722eff0efbd449c9b
Milind Sharma <milindsharma8@gmail.com>
2021-11-04 12:19:22 +0800

    Move get_headers to functions, change usage in scraper

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import openpyxl
+from typing import Dict, Tuple
 
 
 def fix_ticker_formatting(filename: str,
@@ -18,6 +19,20 @@ def fix_ticker_formatting(filename: str,
         
         cell.value = '.'.join(ticker_data) # Adding the . while joining them
     workbook.save(filename=save_filename)
+
+
+def get_headers() -> Dict[str, str]:
+    return {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+    "cache-control": "max-age=0",
+    "dnt": "1",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"}
 
 
 if __name__ == '__main__':
