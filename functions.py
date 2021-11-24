5a12276e62c6821e36e11bc80a95413dcd6a4260
Milind Sharma <milindsharma8@gmail.com>
2021-11-24 20:53:34 +0800

    Reformat functions

--- functions.py
+++ functions.py
@@ -94,25 +94,6 @@ def generate_rand_delay(upper: int = 10,
     time.sleep(random.randint(lower, upper))
 
 
-def _get_headers() -> Dict[str, str]:
-    # Returns headers that allow you to scrape Yahoo finance.
-    return {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image"
-                  "/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
-        "cache-control": "max-age=0",
-        "dnt": "1",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "none",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/"
-                      "537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
-        }
-
-
 def get_debt_shares(bal_url: str,
                     ticker: str,
                     ) -> Tuple[float, float, float, float]:
@@ -332,6 +313,25 @@ def get_hist_price(price_url: str,
         _log_error(price_url, ticker, place='price17')
 
     return (price20, price19, price18, price17)
+
+
+def _get_headers() -> Dict[str, str]:
+    # Returns headers that allow you to scrape Yahoo finance.
+    return {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image"
+                  "/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,ml;q=0.7",
+        "cache-control": "max-age=0",
+        "dnt": "1",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/"
+                      "537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+        }
 
 
 def _log_error(url: str, ticker: str, place: str) -> None:
